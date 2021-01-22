bc277151ad738201de0b82167a8f780e1c83a842
Kim-SiHwan <tjdans4166@naver.com>
2021-01-22 22:11:27 +0900

    회원가입 Dto

---
+++ src/main/java/com/example/boardbvsj/dto/JoinDto.java
@@ -0,0 +1,29 @@
+package com.example.boardbvsj.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JoinDto {
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String nickname;
+}
