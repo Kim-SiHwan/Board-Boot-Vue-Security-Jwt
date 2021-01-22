e47889840644c78dd3b6942f7fc5ad3ae001c351
Kim-SiHwan <tjdans4166@naver.com>
2021-01-22 22:12:25 +0900

    유저 Entity

---
+++ src/main/java/com/example/boardbvsj/entity/Member.java
@@ -0,0 +1,37 @@
+package com.example.boardbvsj.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    @JsonIgnore
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(name = "username", length = 50, unique = true)
+    private String username;
+
+    @JsonIgnore
+    @Column(name = "password", length = 100)
+    private String password;
+
+    @Column(name = "nickname", length = 50)
+    private String nickname;
+
+    @Column(name = "roles")
+    public String role;
+
+
+}
