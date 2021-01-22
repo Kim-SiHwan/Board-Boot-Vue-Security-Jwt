d1f9137c62265e59693bcc64318e4baa054631a9
Kim-SiHwan <tjdans4166@naver.com>
2021-01-22 22:07:43 +0900

    start

---
+++ src/main/java/com/example/boardbvsj/BoardBvsjApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardbvsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardBvsjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardBvsjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boardbvsj/BoardBvsjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardbvsj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardBvsjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
