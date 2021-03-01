5003060e9816629a0527d182d87cf279ccd3a8cf
Kim-SiHwan <tjdans4166@naver.com>
2021-03-02 00:27:38 +0900

    회원 리팩토링

--- src/main/java/com/example/boardbvsj/controller/MemberController.java
+++ src/main/java/com/example/boardbvsj/controller/MemberController.java
@@ -7,26 +7,26 @@ import com.example.boardbvsj.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/member")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
 
+    @GetMapping("/test")
+    public String test(){
+        return "test!~!";
+    }
 
     @PostMapping("/save")
     public void save(@RequestBody JoinDto dto){
         memberService.save(dto);
     }
 
-    @PostMapping("/authenticate")
+    @PostMapping("/login")
     public ResponseEntity<MemberResponseDto> authorize(@RequestBody LoginDto loginDto){
-
         return new ResponseEntity<>(memberService.login(loginDto),HttpStatus.OK);
     }
 
